54c800915e29efb7314db50d8b434e18a60aed25
Tristan Ashley <trisashley@gmail.com>
2018-06-03 19:42:18 +0100

    feat: Add input element, type and if required to input object

--- scripts/input.js
+++ scripts/input.js
@@ -4,7 +4,19 @@
     function inputManager(elemInput) {
         _elemInput = elemInput;
 
-        return elemInput;
+        return {
+            element: elemInput,
+            type: getInputType(),
+            isRequired: isInputRequired()
+        };
+    }
+
+    function isInputRequired() {
+        return _elemInput.hasAttribute('required');
+    }
+
+    function getInputType() {
+        return _elemInput.getAttribute('type');
     }
 
     window.inputManager = inputManager;
