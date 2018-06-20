63a277cd2d67a5f6c027f3f69fdb85e80acb99b2
Tris <trisashley@gmail.com>
2018-06-20 13:05:42 +0100

    refactor: Remove getForms() from API, may add it back in later

--- src/main.js
+++ src/main.js
@@ -18,12 +18,6 @@ function addForm(config) {
     forms.push(formManager(config));
 }
 
-function getForms() {
-    console.log('forms', forms);
-    return forms;
-}
-
 window.formValidator = {
-    addForm,
-    getForms
+    addForm
 }
