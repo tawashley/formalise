cabf6cebba41fb9496353931397481f794f12301
Tris <tristan.ashley@codecomputerlove.com>
2018-12-05 13:31:27 +0000

    test: Add first set of tests to input.js

---
+++ src/test/input.test.js
@@ -0,0 +1,54 @@
+import inputManager from 'input';
+
+function createInputElement() {
+    const input = document.createElement('input');
+
+    input.type = 'text';
+
+    return input;
+}
+
+function getDefaultConfigObject() {
+    return {
+        form: null,
+        validateOn: {
+            blur: true
+        },
+        submitFormWhenValid: false,
+        onInputBlur: function() {},
+        onFormSubmit: function() {},
+        inputParentSelector: null,
+        focusOnFirstInvalidInput: true
+    }
+}
+
+function getConfigObject(config = {}) {
+    return Object.assign({}, getDefaultConfigObject(), config);
+}
+
+describe('inputManager', () => {
+    test('exports a function', () => {
+        expect(typeof inputManager).toBe('function');
+    });
+
+    describe('When creating an instance of an input', () => {
+        test('returns the public api object', () => {
+            const inputInstance = inputManager(createInputElement(), getConfigObject());
+
+            expect(inputInstance.element).not.toBeNull();
+            expect(inputInstance.validate).not.toBeNull();
+        });
+
+        test('returns a HTMLInputElement', () => {
+            const inputInstance = inputManager(createInputElement(), getConfigObject());
+
+            expect(inputInstance.element instanceof HTMLInputElement).toBe(true);
+        });
+
+        test('add the class "is-pristine" to the element after creation', () => {
+            const inputInstance = inputManager(createInputElement(), getConfigObject());
+
+            expect(inputInstance.element.classList.contains('is-pristine')).toBe(true);
+        })
+    })
+});
