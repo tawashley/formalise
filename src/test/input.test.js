f652b823a04d592c054287ba544ad5ae4ec7c0b0
Tris <tristan.ashley@codecomputerlove.com>
2018-12-05 17:24:02 +0000

    test: Add tests around input validation classes

--- src/test/input.test.js
+++ src/test/input.test.js
@@ -1,9 +1,13 @@
 import inputManager from 'input';
 
-function createInputElement() {
+function createInputElement(required = false) {
     const input = document.createElement('input');
 
     input.type = 'text';
+
+    if(required) {
+        input.required = true;
+    }
 
     return input;
 }
@@ -51,20 +55,64 @@ describe('inputManager', () => {
             expect(inputInstance.element.classList.contains('is-pristine')).toBe(true);
         });
 
-        test('calls the provided "blur" handler if validateOn.blur is set to true', () => {
-            const mockFunction = jest.fn();
+        describe('and validate.blur config is set to true', () => {
+            let mockFunction;
+            let inputInstance;
 
-            const inputInstance = inputManager(createInputElement(), getConfigObject({
-                validateOn: {
-                    blur: true
-                },
-                onInputBlur: mockFunction
-            }));
+            beforeEach(() => {
+                mockFunction = jest.fn();
 
-            inputInstance.element.focus();
-            inputInstance.element.blur();
+                inputInstance = inputManager(createInputElement(), getConfigObject({
+                    validateOn: {
+                        blur: true
+                    },
+                    onInputBlur: mockFunction
+                }));
+            });
 
-            expect(mockFunction.mock.calls.length).toBe(1);
+            test('the "blur" function handler is correctly called', () => {
+                inputInstance.element.focus();
+                inputInstance.element.blur();
+
+                expect(mockFunction.mock.calls.length).toBe(1);
+            })
+
+            test('the correct validiation classes are added to the input element', () => {
+                inputInstance.element.focus();
+                inputInstance.element.blur();
+
+                expect(inputInstance.element.classList.contains('is-dirty')).toBe(true);
+                expect(inputInstance.element.classList.contains('is-valid')).toBe(true);
+            })
         })
+
+        describe('and the input is validated', () => {
+            test('the correct validation classes are add to the element', () => {
+                const inputInstanceRequired = inputManager(createInputElement(true), getConfigObject());
+
+                expect(inputInstanceRequired.element.classList.contains('is-pristine')).toBe(true);
+                expect(inputInstanceRequired.element.classList.contains('is-dirty')).toBe(false);
+
+                inputInstanceRequired.validate();
+
+                expect(inputInstanceRequired.element.classList.contains('is-dirty')).toBe(true);
+                expect(inputInstanceRequired.element.classList.contains('is-invalid')).toBe(true);
+                expect(inputInstanceRequired.element.classList.contains('is-valid')).toBe(false);
+
+                inputInstanceRequired.element.value = 'input value';
+                inputInstanceRequired.validate();
+
+                expect(inputInstanceRequired.element.classList.contains('is-dirty')).toBe(true);
+                expect(inputInstanceRequired.element.classList.contains('is-valid')).toBe(true);
+                expect(inputInstanceRequired.element.classList.contains('is-invalid')).toBe(false);
+
+                inputInstanceRequired.element.value = '';
+                inputInstanceRequired.validate();
+
+                expect(inputInstanceRequired.element.classList.contains('is-dirty')).toBe(true);
+                expect(inputInstanceRequired.element.classList.contains('is-invalid')).toBe(true);
+                expect(inputInstanceRequired.element.classList.contains('is-valid')).toBe(false);
+            })
+        });
     })
 });
