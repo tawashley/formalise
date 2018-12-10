533c403eaa21a4e13957b2b46370b5b38cbca62f
Tris <tristan.ashley@codecomputerlove.com>
2018-12-10 16:02:37 +0000

    build: changes to DOMUtils.js after running eslint --fix + tweaked follwed from this

--- src/DOMUtils.js
+++ src/DOMUtils.js
@@ -2,25 +2,24 @@ export function addClass(element, className) {
     if (element.classList) {
         element.classList.add(className);
     } else {
-        element.className += ' ' + className;
+        element.className += ` ${className}`;
     }
-};
+}
 
 export function removeClass(element, className) {
     if (element.classList) {
         element.classList.remove(className);
     } else {
-        element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        element.className = element.className.replace(new RegExp(`(^|\\b)${className.split(' ').join('|')}(\\b|$)`, 'gi'), ' ');
     }
-};
+}
 
 export function hasClass(element, className) {
     if (element.classList) {
         return element.classList.contains(className);
-    } else {
-        return new RegExp('(^| )' + className + '( |$)', 'gi').test(element.className);
     }
-};
+    return new RegExp(`(^| )${className}( |$)`, 'gi').test(element.className);
+}
 
 export function toggleAttribute(element, attr, val) {
     if (element.hasAttribute(attr) && element.getAttribute(attr) === val) {
@@ -28,14 +27,14 @@ export function toggleAttribute(element, attr, val) {
     } else {
         element.setAttribute(attr, val);
     }
-};
+}
 
 export function toggleClass(element, className) {
     if (element.classList) {
         element.classList.toggle(className);
     } else {
-        var classes = element.className.split(' ');
-        var existingIndex = classes.indexOf(className);
+        const classes = element.className.split(' ');
+        const existingIndex = classes.indexOf(className);
 
         if (existingIndex >= 0) {
             classes.splice(existingIndex, 1);
@@ -45,23 +44,30 @@ export function toggleClass(element, className) {
 
         element.className = classes.join(' ');
     }
-};
+}
 
 export function removeElement(element) {
     element.parentNode.removeChild(element);
 }
 
 export function getClosestElement(element, selector) {
-    if(element.closest) {
-        return element.closest(selector);
-    } else {
-        if (!document.documentElement.contains(element)) return null;
-        
-        do {
-            if (element.matches(selector)) return element;
-            element = element.parentElement || element.parentNode;
-        } while (element !== null && element.nodeType === 1); 
+    let baseElement = element;
 
+    if (baseElement.closest) {
+        return baseElement.closest(selector);
+    }
+
+    if (!document.documentElement.contains(baseElement)) {
         return null;
     }
+
+    do {
+        if (baseElement.matches(selector)) {
+            return baseElement;
+        }
+
+        baseElement = baseElement.parentElement || baseElement.parentNode;
+    } while (baseElement !== null && baseElement.nodeType === 1);
+
+    return null;
 }
