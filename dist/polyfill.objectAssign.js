48bc7510600e089809dd71e881739bfe49077570
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-15 13:46:29 +0000

    Deploy tawashley/formalise to github.com/tawashley/formalise.git:gh-pages

---
+++ dist/polyfill.objectAssign.js
@@ -0,0 +1,1 @@
+"function"!=typeof Object.assign&&Object.defineProperty(Object,"assign",{value:function(e,t){"use strict";if(null==e)throw new TypeError("Cannot convert undefined or null to object");for(var n=Object(e),r=1;r<arguments.length;r++){var o=arguments[r];if(null!=o)for(var c in o)Object.prototype.hasOwnProperty.call(o,c)&&(n[c]=o[c])}return n},writable:!0,configurable:!0});
